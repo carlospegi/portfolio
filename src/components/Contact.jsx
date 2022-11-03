b1559e695d36fbfb23287032aaccab1e965c35f2
carlos <carlos.pegi.01@gmail.com>
2022-11-03 07:51:37 -0700

    Footer

---
+++ src/components/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div name="contact" className='w-full h-screen bg-[#0a192f] flex justify-center items-center ' >
+    <form method='POST' action="https://getform.io/f/b5b123ca-00d1-4549-be96-aa9ce6bed38b" className='flex flex-col max-w-[600px] w-full' >
+        <div className='pb-8' >
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300' >Contact</p>
+            <p className='text-gray-300 py-4' >Submit the form bellow or shoot me an email - carlos.pegi.01@gmail.com</p>
+        </div>
+        <input className='p-2 bg-[#ccd6f6]' type="text" placeholder='Name' name='name' />
+        <input className='my-4 p-2 bg-[#ccd6f6]' type="text" placeholder='Email' name='email' />
+        <textarea className='p-2 bg-[#ccd6f6]' name="message" id="" cols="30" rows="10" placeholder='Message'></textarea>
+        <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center duration-500' >Let's Collaborate</button>
+    </form>
+      
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from 'react'
+import { FaTimes, FaBars, FaGithub, FaLinkedinIn } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+import Logo from '../assets/logo1.png'
+import { Link } from 'react-scroll'
+
+const Navbar = () => {
+
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => {
+    setNav(!nav)
+  }
+
+  return (
+    <div className='flex w-full h-[80px] justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+      <div>
+        <img src={Logo} alt="logo" style={{ width: '50px' }} />
+      </div>
+      {/* menu */}
+
+
+      <ul className='hidden md:flex' >
+        <li>
+          <Link className='md:px-4' to='home' smooth={true} duration={500} >
+            Home
+          </Link>
+          <Link className='md:px-4' to='about' smooth={true} duration={500} >
+            About
+          </Link>
+          <Link className='md:px-4' to='skills' smooth={true} duration={500} >
+            Skills
+          </Link>
+          <Link className='md:px-4' to='work' smooth={true} duration={500} >
+            Work
+          </Link>
+          <Link className='md:px-4' to='contact' smooth={true} duration={500} >
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+
+      {/* Hamburger */}
+      <div className='md:hidden z-10'
+        onClick={handleClick}
+      >
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* Mobile Menu */}
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'} >
+        <li className='py-6 text-4xl' >
+          <Link onClick={handleClick} to='home' smooth={true} duration={500} >
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl' >
+        <Link onClick={handleClick} to='about' smooth={true} duration={500} >
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl' >
+        <Link onClick={handleClick} to='skills' smooth={true} duration={500} >
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl' >
+        <Link onClick={handleClick} to='work' smooth={true} duration={500} >
+            Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl' >
+        <Link onClick={handleClick}to='contact' smooth={true} duration={500} >
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+
+      {/* Social Icons */}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0' >
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-blue-600 '>
+            <a
+              className='flex w-full items-center justify-between text-gray-300 '
+              href="/">Linkedin
+              <FaLinkedinIn size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-[#333333] '>
+            <a
+              className='flex w-full items-center justify-between text-gray-300 '
+              href="/">Github
+              <FaGithub size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-[#376c6b]'>
+            <a
+              className='flex w-full items-center justify-between text-gray-300 '
+              href="/">Email
+              <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-500 bg-[#5e5e46] '>
+            <a
+              className='flex w-full items-center justify-between text-gray-300 '
+              href="/">Resume
+              <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
